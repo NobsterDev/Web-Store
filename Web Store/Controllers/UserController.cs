a70c62d78dcd2ee1c6f4e5ab4eb25ac50fac99f7
NobsterLaptop <furkanka10@gmail.com>
2019-06-17 20:32:57 +0200

    db error

--- Web Store/Controllers/UserController.cs
+++ Web Store/Controllers/UserController.cs
@@ -44,13 +44,65 @@ namespace Web_Store.Controllers
         // POST: Product/Edit/5
         [Route("Profile")]
         [HttpPost]
-        public ActionResult ProfileDetails( user user)
+        public ActionResult ProfileDetails(user user)
         {
             try { auth(); } catch { return Redirect("/"); }
-
-
+            //?
             return RedirectToAction("Index");
         }
+
+        // GET: Product
+        [Route("Products")]
+        public ActionResult Products()
+        {
+            try { auth(); } catch { return Redirect("/"); }
+            List<product> plist = new List<product>();
+            user tmpuser = Session["user"] as user;
+            List<cart> clist = new List<cart>();
+            clist = db.cart.Where(x=> x.User_idUsers == tmpuser.idUsers).ToList();
+            plist = db.product.ToList();
+            List<product> tmpplist = new List<product>();
+            foreach (var c in clist)
+            {
+                foreach (var p in plist)
+                {
+                    if (p.idItems == c.Product_idItems)
+                    {
+                        tmpplist.Add(p);
+                    }      
+                }
+            }
+            foreach (var item in tmpplist)
+            {
+                plist.Remove(item);
+            }
+            return View(plist);
+        }
+
+        // GET: addcart
+        [Route("AddCart/{id}")]
+        public ActionResult Addtocart(int id)
+        {
+            try { auth(); } catch { return Redirect("/"); }
+            try
+            {
+                cart cart = new cart();
+                user tmpuser = Session["user"] as user;
+                cart.User_idUsers = tmpuser.idUsers;
+                cart.Product_idItems = id;
+                cart.Quantity = 0;
+                db.cart.Add(cart);
+                db.SaveChanges();
+                TempData["AlertMessage"] = "Success";
+                return RedirectToAction("Products");
+            }
+            catch
+            {
+                TempData["AlertMessage"] = "Failed";
+                return RedirectToAction("Products");
+            }
+        }
+
 
         private void auth()
         {
